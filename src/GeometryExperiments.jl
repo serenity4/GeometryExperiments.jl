3781c079c3ce32c184b56abaa5aaaeb8ce0665ee
serenity4 <cedric.bel@hotmail.fr>
2021-04-05 14:26:42 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.16

---
+++ src/GeometryExperiments.jl
@@ -0,0 +1,5 @@
+module GeometryExperiments
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using GeometryExperiments
+using Test
+
+@testset "GeometryExperiments.jl" begin
+    # Write your tests here.
+end
