c2d2ff472728ed1ca05d1e2f96a5286133f98c23
serenity4 <cedric.bel@hotmail.fr>
2021-04-09 23:28:44 +0200

    Add doc and constructor to Ellipsoid

--- src/primitives.jl
+++ src/primitives.jl
@@ -12,8 +12,21 @@ norm(p::Point, ::Type{<:NormedPrimitive{N}}) where {N} = norm(coordinates(p), N)
 const HyperSphere{T} = NormedPrimitive{2,T}
 const HyperCube{T} = NormedPrimitive{Inf,T}
 
+"""
+    Ellipsoid(semiaxes)
+
+An ellipsoid with semi-axes `semiaxes`.
+Is equivalent to a scaled [`HyperSphere`](@ref).
+"""
 const Ellipsoid{Dim,T} = Scaled{HyperSphere{T},Dim,T}
 Ellipsoid(radius::T, transf::Scaling{Dim,T}) where {Dim,T} = Ellipsoid{Dim,T}(HyperSphere(radius), transf)
+
+function Ellipsoid(semiaxes)
+  n = norm(axes)
+  Ellipsoid(n, axes ./ n)
+end
+
+Base.show(io::IO, elps::Ellipsoid{Dim,T}) where {Dim,T} = print(io, "Ellipsoid{$Dim, $T}($(elps.transf.vec .* elps.obj.radius))")
 
 # A box around the origin. This is not the same as `Meshes.Box`, it is missing the translation part.
 const Box{Dim,T} = Scaled{HyperCube{T},Dim,T}
