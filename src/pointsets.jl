39998e177c1c9faba4fe4d2e157712a85c59ec62
serenity4 <cedric.bel@hotmail.fr>
2021-06-05 16:18:31 +0200

    Small refactor

--- src/pointsets.jl
+++ src/pointsets.jl
@@ -9,10 +9,9 @@ end
 
 function boundingelement(set::PointSet{Dim}) where {Dim}
     c = centroid(set)
-    loc = Translation(c)
     coords = map(1:Dim) do i
         maximum(getindex.(set.points, i))
     end
     scale = Scaling(coords - c)
-    Translated(Scaled(HyperCube(1.), scale), loc)
+    Translated(Scaled(HyperCube(1.), scale), Translation(c))
 end
